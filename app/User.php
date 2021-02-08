6c82a649c95bf62eb7d34157bba70ea03a553862
m-rukhshona-a <j.rukhshona@gmail.com>
2021-02-08 17:50:18 +0500

    Login page

--- app/User.php
+++ app/User.php
@@ -20,6 +20,8 @@ class User extends Authenticatable
         'name', 'email', 'password',
     ];
 
+    protected $table = 'users';
+
     protected $hidden = [
         'password', 'remember_token',
     ];
